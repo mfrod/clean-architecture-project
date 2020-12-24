cd2179957106cdab8dbf5dca376385da78421eae
mfrod <mf.rodrigues@unifesp.br>
2020-12-23 21:37:24 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,11 +1,24 @@
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe ('User Domain class', () =>{
     test('should not create user with invalid email address', () => {
         const invalidEmail = 'invalid_email'
         const error = User.create( {name: 'any_name', email: invalidEmail} )
         expect(error).toEqual(left(new InvalidEmailError()))
+    })
+
+    test ('should not create user with invalid name (few characters', () =>{
+        const invalidName = '0      '
+        const error = User.create({ name: invalidName, email:'any@email.com' })
+        expect(error).toEqual(left(new InvalidNameError()))
+    })
+
+    test ('should not create user with invalid name (too many characters)', () =>{
+        const invalidName = '0'.repeat(257)
+        const error = User.create({ name: invalidName, email:'any@email.com' })
+        expect(error).toEqual(left(new InvalidNameError()))
     })
 })
