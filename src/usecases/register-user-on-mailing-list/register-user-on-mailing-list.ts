0bfd3bd49bbb97f299ffa00d526c4ca1823ada1d
mfrod <mf.rodrigues@unifesp.br>
2021-02-08 22:19:01 -0300

    refactor: introduce use case interface

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,16 +1,16 @@
 import { InvalidNameError, InvalidEmailError } from "@/entities/errors";
 import { User, UserData } from "@/entities";
 import { Either, left, right } from "@/shared";
-import { UserRepository } from "@/usecases/register-user-on-mailing-list/ports";
+import { UseCase, UserRepository } from "@/usecases/register-user-on-mailing-list/ports";
 
-export class RegisterUserOnMailingList {
-    private  readonly userRepo: UserRepository
+export class RegisterUserOnMailingList implements UseCase {
+    private readonly userRepo: UserRepository
 
     construtor (userRepo: UserRepository) {
         this.userRepo = userRepo
     }
 
-    public async RegisterUserOnMailingList (request: UserData): 
+    public async perform (request: UserData): 
         Promise<Either<InvalidNameError | InvalidNameError, UserData>> {
         const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
         if(userOrError.isLeft()) {
