9cde98b525780f40b426c930ec0e075808de9f75
mfrod <mf.rodrigues@unifesp.br>
2021-02-18 05:21:43 -0300

    refactor: move middleware and routes folders inside main folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,6 +7,6 @@ export default (app: Express): void => {
     const router = Router()
     app.use('/api', router)
     readdirSync(`${__dirname}/../routes`).map(async file => {
-        (await import(`${__dirname}/routes/${file}`)).default(router)
+        (await import(`${__dirname}/../routes/${file}`)).default(router)
     })
 }
